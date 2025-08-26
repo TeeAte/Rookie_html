4c22d51a4a553fdc1ef185a35d8bd6a53660f9e0
TeeAte <luwenkeng@foxmail.com>
2025-08-26 16:40:36 +0800

    8

--- Tlias/Tlias.js
+++ Tlias/Tlias.js
@@ -1,3 +1,5 @@
+import { printLog } from "./utils.js"
+
 //删除 按钮的js脚本如下，要求按下删除后，那一行直接消失
 document.querySelectorAll('.del').forEach(function(btn){
     btn.addEventListener('click',function(){
@@ -15,6 +17,6 @@ document.querySelectorAll('.del').forEach(function(btn){
 
 document.querySelector('.name').addEventListener('focus',
     function(){
-        console.log(">>>输入姓名中...")
+        printLog(">>>输入姓名中...")
     }
 )
---
+++ Tlias/utils.js
@@ -0,0 +1,4 @@
+
+export function printLog(msg){
+  console.log(msg);
+}
